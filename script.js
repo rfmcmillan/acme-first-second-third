63c7acd0c8155683f866bd3338b81e3df002d01d
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-02-05 11:14:17 -0500

    refactored event listeners for move right and move left buttons

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const slots = ['first', 'second', 'third'];
 const users = [
   { id: 1, name: 'Moe', slot: 'second' },
   { id: 2, name: 'Larry', slot: 'second' },
-  { id: 3, name: 'Curly', slot: 'first' },
-  { id: 4, name: 'Lucy', slot: 'first' },
+  { id: 3, name: 'Curly', slot: 'second' },
+  { id: 4, name: 'Lucy', slot: 'second' },
 ];
 
 function createUser(user) {
@@ -35,9 +35,29 @@ function toggleSelected(event) {
   }
 }
 
+function moveUsers(sourceSlotId, targetSlotId) {
+  const sourceSlot = document.querySelector(`#${sourceSlotId}`);
+  const targetSlot = document.querySelector(`#${targetSlotId}`);
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].selected) {
+      if (users[i].slot === sourceSlot.id) {
+        users[i].slot = targetSlot.id;
+        const sourceChildren = [...sourceSlot.children];
+        for (let j = 0; j < sourceChildren.length; j++) {
+          if ([...sourceChildren[j].classList].includes('selected')) {
+            sourceSlot.removeChild(sourceChildren[j]);
+            targetSlot.appendChild(sourceChildren[j]);
+          }
+        }
+      }
+    }
+  }
+}
+
+// call placeUsers to initially place the users
 placeUsers();
 
-// add event listeners to user elements
+// add event listeners to user elements_____________________
 
 const userElems = Array.from(document.querySelectorAll('.user'));
 
@@ -55,75 +75,17 @@ const moveLeftBtn2 = document.getElementById('move-left-2');
 const moveLeftBtn3 = document.getElementById('move-left-3');
 
 moveRightBtn1.addEventListener('click', () => {
-  const firstSlot = document.querySelector('#first');
-  const secondSlot = document.querySelector('#second');
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].selected) {
-      if (users[i].slot === 'first') {
-        users[i].slot = 'second';
-        const firstChildren = [...firstSlot.children];
-        for (let j = 0; j < firstChildren.length; j++) {
-          if ([...firstChildren[j].classList].includes('selected')) {
-            firstSlot.removeChild(firstChildren[j]);
-            secondSlot.appendChild(firstChildren[j]);
-          }
-        }
-      }
-    }
-  }
+  moveUsers('first', 'second');
 });
 
 moveRightBtn2.addEventListener('click', () => {
-  const thirdSlot = document.querySelector('#third');
-  const secondSlot = document.querySelector('#second');
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].selected) {
-      if (users[i].slot === 'second') {
-        users[i].slot = 'third';
-        const secondChildren = [...secondSlot.children];
-        for (let j = 0; j < secondChildren.length; j++) {
-          if ([...secondChildren[j].classList].includes('selected')) {
-            secondSlot.removeChild(secondChildren[j]);
-            thirdSlot.appendChild(secondChildren[j]);
-          }
-        }
-      }
-    }
-  }
+  moveUsers('second', 'third');
 });
 
 moveLeftBtn2.addEventListener('click', () => {
-  const firstSlot = document.querySelector('#first');
-  const secondSlot = document.querySelector('#second');
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].slot === 'second' && users[i].selected) {
-      users[i].slot = 'first';
-      const secondChildren = [...secondSlot.children];
-      for (let j = 0; j < secondChildren.length; j++) {
-        if ([...secondChildren[j].classList].includes('selected')) {
-          console.log('match');
-          secondSlot.removeChild(secondChildren[j]);
-          firstSlot.appendChild(secondChildren[j]);
-        }
-      }
-    }
-  }
+  moveUsers('second', 'first');
 });
 
 moveLeftBtn3.addEventListener('click', () => {
-  const thirdSlot = document.querySelector('#third');
-  const secondSlot = document.querySelector('#second');
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].slot === 'third' && users[i].selected) {
-      users[i].slot = 'second';
-      const thirdChildren = [...thirdSlot.children];
-      for (let j = 0; j < thirdChildren.length; j++) {
-        if ([...thirdChildren[j].classList].includes('selected')) {
-          console.log('match');
-          thirdSlot.removeChild(thirdChildren[j]);
-          secondSlot.appendChild(thirdChildren[j]);
-        }
-      }
-    }
-  }
+  moveUsers('third', 'second');
 });
